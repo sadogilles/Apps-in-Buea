296f4860c53abe97fdcffbf4013453cb9e80ed91
sadogilles <sadowizzy@gmail.com>
2016-12-10 12:55:03 +0000

    Add files via upload

---
+++ Apps In Buea/index.php
@@ -0,0 +1,127 @@
+<!--doctype Html -->
+<html>
+	<head>
+		<!--charset respond -->
+		<meta charset="utf-8"/>
+		<title>Apps In Buea</title>
+		<!--enable view-point regulation -->
+		<meta name="viewpoint" content="width=device-width, initial-scale=1.0" />
+		<!-- custom-css -->
+		<link  href="css/custom.css" rel="stylesheet"/>
+		<!-- Bootstrap -->
+		<link  href="css/bootstrap.min.css" rel="stylesheet"/>
+		<script src="js/respond.js" ></script>
+		<!-- jquery js-->
+		<script src="js/jquery-3.1.1.min.js"></script>
+	</head>
+	
+	<body>
+		<div class="loader"></div>
+		<header id="header" class=" row jumbotron">
+			<div id="heading" class="col-lg-10"><h1>Apps In Buea</h1></div>
+			<div id="Add-button" class="col-lg-2"><input type="button" class="btn btn-success  btn-lg" value="Add An App"/></div>
+		</header>
+		
+		<div id="container " class="jumbotron">
+			<div class="row">
+				<div id="" class="col-lg-3 col-sm-6 app ">
+					<div id="logo" class="img-responsive">
+					logo-here
+					</div>
+					<div id="app-name" class="">
+					app-name-here
+					</div>
+					<div id="description" class="caption">
+					description-here
+					</div>
+				</div>
+				<div id="" class=" col-lg-3 col-sm-6 app">
+					<div id="logo" class="img-responsive">
+					logo-here
+					</div>
+					<div id="app-name" class="">
+					app-name-here
+					</div>
+					<div id="description" class="caption">
+					description-here
+					</div>
+				</div>
+				<div id="" class="col-lg-3 col-sm-6 app">
+					<div id="logo" class="img-responsive">
+					logo
+					</div>
+					<div id="app-name" class="">
+					app-name
+					</div>
+					<div id="description" class="caption">
+					description
+					</div>
+				</div>
+				<div id="" class="col-lg-3 col-sm-6 app">
+					<div id="logo" class="img-responsive">
+					logo
+					</div>
+					<div id="app-name" class="">
+					app-name
+					</div>
+					<div id="description" class="caption">
+					description
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div id="" class="col-lg-3 col-sm-6 app">
+					<div id="logo" class="">
+					logo-here
+					</div>
+					<div id="app-name" class="">
+					app-name
+					</div>
+					<div id="description" class="">
+					description
+					</div>
+				</div>
+				<div id="" class=" col-lg-3 col-sm-6 app">
+					<div id="logo" class="">
+					logo
+					</div>
+					<div id="app-name" class="">
+					app-name
+					</div>
+					<div id="description" class="caption">
+					description
+					</div>
+				</div>
+				<div id="" class="col-lg-3 col-sm-6 app">
+					<div id="logo" class="">
+					logo
+					</div>
+					<div id="app-name" class="">
+					app-name
+					</div>
+					<div id="description" class="caption">
+					description
+					</div>
+				</div>
+				<div id="" class="col-lg-3 col-sm-6 app">
+					<div id="logo" class="">
+					logo
+					</div>
+					<div id="app-name" class="">
+					app-name
+					</div>
+					<div id="description" class="caption">
+					description
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<footer class="jumbotron row">
+			<marquee direction="up"><center><small><address>Buea, Camerooun <br>Molyko,malingo<br>God's Grace Hostel <br>Tel:656006540</marquee></center></address></small>
+		</footer>
+		
+		<!-- Bootstrap  js-->
+		<script src="js/boostrap.min.js"></script>
+	</body>
+</html>
